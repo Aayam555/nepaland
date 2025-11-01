cc19fc8f243e5ace4ff180b0ef2d5fd9f48f402a
Aayam Shrestha <aayamthebest555@gmail.com>
2025-11-01 07:37:15 +0545

    initial changes 2

---
+++ app.js
@@ -0,0 +1,12 @@
+function create(TOC, images, data, sources){
+    let TOC = TOC;
+    let images = images;
+    let data = data;
+    let sources = sources;
+
+    let TOCstring = "";
+
+    let datastring = "";
+
+    
+}
